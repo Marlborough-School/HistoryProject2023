bd37510b0bd018c76643eac5714d83eb237cc5da
SamMarlborough24 <132004040+SamMarlborough24@users.noreply.github.com>
2023-04-27 11:28:43 -0700

    Beta change

--- sam/js/script.js
+++ sam/js/script.js
@@ -84,7 +84,7 @@ monogatari.assets ('scenes', {
 monogatari.script ({
 	'Start': [
 		'show background duck with fadeIn',
-		'Steve Hideg used to be a very adventurous young boy who always found joy in everything he did. ',
+		'sam has changed this. ',
 		'Example question: Hey bum bum bum got any grapes?',
 		{'Choice':{
 			'Y':{
