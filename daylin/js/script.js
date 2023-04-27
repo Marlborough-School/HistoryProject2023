5d4f310f4da53df496d8d1eca978cad434d17ec0
daylinsk <daylinkaplan24@marlborough.org>
2023-04-27 11:42:52 -0700

    crycat

--- daylin/js/script.js
+++ daylin/js/script.js
@@ -83,7 +83,7 @@ monogatari.assets ('scenes', {
 //
 monogatari.script ({
 	'Start': [
-		'show background duck with fadeIn',
+		'show background crycat with fadeIn',
 		'Steve Hideg used to be a very adventurous young boy who always found joy in everything he did. ',
 		'Example question: Hey bum bum bum got any grapes?',
 		{'Choice':{
