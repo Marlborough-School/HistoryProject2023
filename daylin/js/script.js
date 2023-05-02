50ce572f356cb94eed1c8967006be389e8581053
daylinsk <daylinkaplan24@marlborough.org>
2023-05-02 13:39:18 -0700

    restart

--- daylin/js/script.js
+++ daylin/js/script.js
@@ -84,7 +84,7 @@ monogatari.assets ('scenes', {
 monogatari.script ({
 	'Start': [
 		'show background crycat with fadeIn',
-		'Steve Hideg used to be a very adventurous young boy who always found joy in everything he did. ',
+		'Chiune sugihara was a japanese diplomat who saved the lives of thousands of jewish refugees during World War II.',
 		'Example question: Hey bum bum bum got any grapes?',
 		{'Choice':{
 			'Y':{
