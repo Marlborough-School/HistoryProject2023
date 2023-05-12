15c94bcbaa690b9ea96f55dffc26f1b3e3314ef0
graceplunk <132398201+graceplunk@users.noreply.github.com>
2023-05-12 13:12:13 -0700

    n

--- grace/js/script.js
+++ grace/js/script.js
@@ -107,7 +107,7 @@ monogatari.script ({
 
 	'noAnswer':[
 		'show background black',
-		'He began this military career when he served in World War I, and was promoted to Captain during the Battle of Verdun where he was injured and taken captive. After this event he made tactical suggestions that were turned down by his higher-ups, but that was only the beginning for his military career. In between the first and second world war, Charles de Gaulle continued his education more formally at a military school until France was invaded by Nazi Germany in around 1939, when the second world war had begun.',
+		'He began his military career when he served in World War I, and was promoted to Captain during the Battle of Verdun where he was injured and taken captive. After this event he made tactical suggestions that were turned down by his higher-ups, but that was only the beginning for his military career. In between the first and second world war, Charles de Gaulle continued his education more formally at a military school until France was invaded by Nazi Germany in around 1939, when the second world war had begun.',
 		'jump choiceScreen',
 	],
 
