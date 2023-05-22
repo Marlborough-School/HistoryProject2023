eab6dbfaba3ce9e99ecd59dd3b09d73e25adfd34
isabel223489 <132004125+isabel223489@users.noreply.github.com>
2023-05-22 08:58:39 -0700

    u

--- isabel/js/script.js
+++ isabel/js/script.js
@@ -61,6 +61,7 @@ monogatari.assets ('sounds', {
 monogatari.assets ('videos', {
     'cat-video':'cat-2879.mp4',
     'LILYVIDEO':'LILYVIDEO.mov',
+    'MYMOVIE':'MYMOVIE.mp4',
     
 });
 
@@ -207,8 +208,12 @@ monogatari.script ({
         'show background black with fadeIn',
         'show image LILY15 with fadeIn',
         'Sharing her story to Tik Tok was Lilys mission and purpose. As antisemitism becomes a rising problem again in the United States, Ebert’s tik tok videos share important messages in an attempt to “save the world” from anti semitic views and negative stereotypes. In an interview, Ebert says “I was really not sure that I would stay alive. It is a miracle that I am here. But I promised myself, however long I will be alive, and whatever I will do in life, one thing is sure, I will tell my story,”',
-        
+        'hide image LILY15',
+        'show background black with fadeIn',
+        'show video MYMOVIE',
         'In one of her tik toks, she shows her pendant, stating that it was the only piece of gold that survived auschwitz.',
+        'hide video MYMOVIE',
+        'end',
       
 		'jump Ending',
 	],
