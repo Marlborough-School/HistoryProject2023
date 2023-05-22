383aae54ee08d5d11011b57d1fa5b7dabcf7fc38
ALANAUSER <alanalaurie24@marlborough.org>
2023-05-22 14:25:02 -0700

    Update script.js

--- alana/js/script.js
+++ alana/js/script.js
@@ -230,10 +230,10 @@ monogatari.script ({
         'I hope you have learn something about the incredible work Erna Gans did for Holocaust Education.',
         'jump bibliography',
     ],
+   
     'bibliography':[
-        'show background black',
+       'show background black',
         'show image bib',
-        'jump citations',
     ],
    
 });
